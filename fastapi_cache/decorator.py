1d00f77a84ed3286649147f31c5fc532af9937b5
Taylor Hubbard <ts.hubbard@gmail.com>
2022-09-01 12:24:37 -0600

    add support for post request type

--- fastapi_cache/decorator.py
+++ fastapi_cache/decorator.py
@@ -58,7 +58,7 @@ def cache(
                 await backend.set(cache_key, coder.encode(ret), expire or FastAPICache.get_expire())
                 return ret
 
-            if request.method != "GET":
+            if request.method not in ['GET', 'POST']:
                 return await func(request, *args, **kwargs)
             if_none_match = request.headers.get("if-none-match")
             if ret is not None:
